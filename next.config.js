41adb1aa69fbb5ca7b002c9cf0c548764399756c
luolp <1143079030@qq.com>
2023-06-01 10:41:40 +0800

    解决图片在前台无法显示的问题

--- next.config.js
+++ next.config.js
@@ -8,9 +8,12 @@ module.exports = {
     appDir: true
   },
   images: {
-    // formats: ['image/avif', 'image/webp'],
-    domains: [
-        'data.eminstore.com'
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'data.eminstore.com'
+      }
     ]
   }
 };
